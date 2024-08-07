b9202d9c31b771e67c874361f3ad526ccb4afc89
Libor Gabaj <libor.gabaj@gmail.com>
2024-08-07 15:28:24 +0200

    Added period for RSSI.

--- config_params_gen.h
+++ config_params_gen.h
@@ -2,7 +2,7 @@
   NAME:
   Labels (names) of generic parameters
 
-  VERSION: 1.8.0
+  VERSION: 1.9.0
 
   DESCRIPTION:
   Generic parameters are utilized almost in every project, especially which
@@ -29,8 +29,9 @@
 #endif
 
 // Periods
-static const char lblPeriodThingspeak[] PROGMEM = "periodThingspeak";
 static const char lblPeriodWifi[] PROGMEM = "periodWifi";
+static const char lblPeriodRssi[] PROGMEM = "periodRssi";
+static const char lblPeriodThingspeak[] PROGMEM = "periodThingspeak";
 static const char lblPeriodRefresh[] PROGMEM = "periodRefresh";
 static const char lblPeriodReconnect[] PROGMEM = "periodReconnect";
 
